0ebffc2ca85d269e36d79397c16630be12f62643
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-05-24 17:43:35 +0530

    rgw_sal_motr : [CORTX-30178] Fix for handling null version-id issues  (#200)
    Problem Statement: Observed the issues of null version-id in PUT, GET, HEAD and List object version API in versioned bucket.
    
    Solution: Generated version-id for null-version obj and created an additional index entry for an object to hold the metadata of null version (Key : obj1[null])
    
    Signed-off-by: Priyanka Salunke <priyanka.s.salunke@seagate.com>

--- src/rgw/rgw_sal_motr.h
+++ src/rgw/rgw_sal_motr.h
@@ -691,6 +691,9 @@ class MotrObject : public Object {
     int delete_part_objs(const DoutPrefixProvider* dpp);
     void set_category(RGWObjCategory _category) {category = _category;}
     int get_bucket_dir_ent(const DoutPrefixProvider *dpp, rgw_bucket_dir_entry& ent);
+    int fetch_null_obj(const DoutPrefixProvider *dpp, bufferlist& bl);
+    int fetch_null_obj_reference(const DoutPrefixProvider *dpp, std::string& prev_null_obj_key);
+    int update_null_reference(const DoutPrefixProvider *dpp, rgw_bucket_dir_entry& ent);
     int update_version_entries(const DoutPrefixProvider *dpp);
     uint64_t get_processed_bytes() { return processed_bytes; }
 };
